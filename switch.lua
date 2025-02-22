1099387669f57f76c49f86c39f032061891ffae0
0wei <142607818+Moe1325@users.noreply.github.com>
2025-02-22 19:37:56 +0800

    switch.lua

---
+++ switch.lua
@@ -0,0 +1,34 @@
+local Player = game:GetService("Players").LocalPlayer
+
+local GunNames = {
+    "AK", "AUGA1", "ARP", "Beretta", "Bullpup", "CZ75", "Deagle", "DoubleBarrel",
+    "Draco", "Famas", "Glock18", "M1Grand", "M4A1", "MAC10", "MAG7", "MP5", "MP7",
+    "MP9", "Negev", "Nova", "P220", "PPBizon", "Pistol", "RPG", "Revolver", "SA58",
+    "SawnOff", "Shotgun", "Uzi"
+}
+
+local AmmoValue = 1000 -- change the number to the ammo count that you wish ;)
+
+local SetAmmo = function(Gun)
+    if not Gun:IsA("Tool") or not table.find(GunNames, Gun.Name) then return end
+
+    local Stats = Gun:FindFirstChild("Stats")
+  
+    if not Stats then return end    
+
+    local Ammo = Stats.Ammo
+
+    if not Ammo then return end
+
+    Ammo.Value = AmmoValue
+end
+
+local OnChildAdded = function(Child)
+    SetAmmo(Child)
+end
+
+Player.Backpack.ChildAdded:Connect(OnChildAdded)
+
+for _, Object in ipairs(Player.Backpack:GetChildren()) do
+    SetAmmo(Object)
+end
